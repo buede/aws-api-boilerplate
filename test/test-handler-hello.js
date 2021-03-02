3c829af21da316b1af5b82be936e71bced923a71
Omar V. Buede <omar@dreangine.xyz>
2021-03-03 00:41:47 +0200

    Added WebSocket example in hello handler

--- test/test-handler-hello.js
+++ test/test-handler-hello.js
@@ -1,19 +1,16 @@
 const { expect } = require('chai');
+const aws = require('aws-sdk-mock');
 const { request: handler } = require('../src/handler/handler-hello');
-
-let event;
-
-beforeEach(() => {
-  // eslint-disable-next-line global-require
-  event = require('./event.json');
-});
+const event = require('./event.json');
+const eventWebSocketConnect = require('./event-websocket-connect.json');
+const eventWebSocketDisconnect = require('./event-websocket-disconnect.json');
+const eventWebSocketMessage = require('./event-websocket-msg.json');
 
 describe('Hello world test', () => {
   it('Return basic message', (done) => {
+    const basicEvent = { ...event, httpMethod: 'GET', resource: '/hello' };
     const expectedBody = JSON.stringify({ msg: 'Hello world!' });
-    event.httpMethod = 'GET';
-    event.resource = '/hello';
-    handler(event)
+    handler(basicEvent)
       .then((response) => {
         const { body } = response;
         expect(body).to.equal(expectedBody);
@@ -22,14 +19,45 @@ describe('Hello world test', () => {
       .catch((error) => done(error));
   });
 
-  it('Unknown path', (done) => {
-    const expectedStatusCode = 404;
-    event.httpMethod = 'GET';
-    event.resource = '/hello/world';
-    handler(event)
+  it('WebSocket connect', (done) => {
+    handler(eventWebSocketConnect)
       .then((response) => {
         const { statusCode } = response;
-        expect(statusCode).to.equal(expectedStatusCode);
+        expect(statusCode).to.equal(200);
+        done();
+      })
+      .catch((error) => done(error));
+  });
+
+  it('WebSocket disconnect', (done) => {
+    handler(eventWebSocketDisconnect)
+      .then((response) => {
+        const { statusCode } = response;
+        expect(statusCode).to.equal(200);
+        done();
+      })
+      .catch((error) => done(error));
+  });
+
+  it('WebSocket message', (done) => {
+    aws.mock('ApiGatewayManagementApi', 'postToConnection', (params, callback) => {
+      callback('', 'mocked');
+    });
+    handler(eventWebSocketMessage)
+      .then((response) => {
+        const { statusCode } = response;
+        expect(statusCode).to.equal(200);
+        done();
+      })
+      .catch((error) => done(error));
+  });
+
+  it('Unknown path', (done) => {
+    const unknownEvent = { ...event, httpMethod: 'GET', resource: '/hello/world' };
+    handler(unknownEvent)
+      .then((response) => {
+        const { statusCode } = response;
+        expect(statusCode).to.equal(404);
         done();
       })
       .catch((error) => done(error));
